6c2b5d127aa7a2ffd83bde4c4679242bf22a12b2
scamlett <scamlett@student.42malaga.com>
2026-03-09 17:56:57 +0100

    Initial import of exam folder

--- Level 00/0-0-aff_a/aff_a.c
+++ Level 00/0-0-aff_a/aff_a.c
@@ -1,18 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   aff_a.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/13 21:38:04 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/13 21:41:27 by fwuensch         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// Write a program that takes a string:
+// and displays the first 'a' character it encounters in it,followed by a newline. 
+//If there are no 'a' characters in the string, the program just writes a newline. 
+//If the number of parameters is not 1, the program displays 'a' followed by a newline.
 
 #include <unistd.h>
 
-int		main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc != 2)
 		write(1, "a", 1);
@@ -23,7 +16,7 @@ int		main(int argc, char **argv)
 			if (*argv[1] == 'a')
 			{
 				write(1, "a", 1);
-				break;
+				break ;
 			}
 			argv[1]++;
 		}
