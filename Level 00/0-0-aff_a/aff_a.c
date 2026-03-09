5bb07d0d79f1dbbed11caabc6495c322b55a7890
scamlett <scamlett@student.42malaga.com>
2026-03-09 19:36:36 +0100

    .

--- Level 00/0-0-aff_a/aff_a.c
+++ Level 00/0-0-aff_a/aff_a.c
@@ -7,20 +7,24 @@
 
 int	main(int argc, char **argv)
 {
+	int	i;
+
 	if (argc != 2)
-		write(1, "a", 1);
+		write(1, "a\n", 2);
 	else
 	{
-		while (*argv[1])
+		i = 0;
+		while (argv[1][i])
 		{
-			if (*argv[1] == 'a')
+			if (argv[1][i] == 'a')
 			{
-				write(1, "a", 1);
-				break ;
+				write(1, "a\n", 2);
+				return (0);
 			}
-			argv[1]++;
+			i++;
 		}
+		write(1, "\n", 1);
 	}
-	write(1, "\n", 1);
 	return (0);
 }
+
