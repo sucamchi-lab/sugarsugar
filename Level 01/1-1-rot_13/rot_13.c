7bc37ba34964e7c35332d97fb90753193f8f3100
scamlett <scamlett@student.42malaga.com>
2026-03-24 15:36:11 +0100

    mejoro ejercicios

--- Level 01/1-1-rot_13/rot_13.c
+++ Level 01/1-1-rot_13/rot_13.c
@@ -2,20 +2,20 @@
 
 int	main(int ac, char **av)
 {
-	int		i;
-	char	c;
+	int	i;
 
 	i = 0;
 	if (ac == 2)
 	{
 		while (av[1][i])
 		{
-			c = av[1][i];
-			if (c >= 'a' && c <= 'z')
-				c = ((c - 'a' + 13) % 26) + 'a';
-			else if (c >= 'A' && c <= 'Z')
-				c = ((c - 'A' + 13) % 26) + 'A';
-			write(1, &c, 1);
+			if ((av[1][i] >= 'a' && av[1][i] <= 'm') || (av[1][i] >= 'A'
+					&& av[1][i] <= 'M'))
+				av[1][i] = av[1][i] + 13;
+			else if ((av[1][i] >= 'n' && av[1][i] <= 'z') || (av[1][i] >= 'N'
+					&& av[1][i] <= 'Z'))
+				av[1][i] = av[1][i] - 13;
+			write(1, &av[1][i], 1);
 			i++;
 		}
 	}
