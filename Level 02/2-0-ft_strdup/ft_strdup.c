659d005a74b3ba18e8d53c08f21c92fb35b2b557
scamlett <scamlett@student.42malaga.com>
2026-03-24 09:31:17 +0100

    corrijo strdup

--- Level 02/2-0-ft_strdup/ft_strdup.c
+++ Level 02/2-0-ft_strdup/ft_strdup.c
@@ -5,31 +5,29 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 		i++;
 	return (i);
 }
 
-char	*ft_strcpy(char *dst, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
 char	*ft_strdup(char *src)
 {
-	char	*dst;
+	int		i;
+	char	*dup;
+	int		len;
 
-	*dst = malloc(sizeof(char) * (ft_strlen(src) + 1));
-	if (dst == NULL)
+	i = 0;
+	len = ft_strlen(src) + 1;
+	dup = malloc(len * sizeof(char));
+	if (dup == NULL)
+	{
 		return (NULL);
-	return (ft_strcpy(dst, src));
+	}
+	while (src[i])
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
 }
